1c489b6bb354c2c054af981ccb1153282b11e767
bender662 <bender662@users.noreply.github.com>
2014-08-10 13:43:58 +0300

    first challenge

---
+++ dailyprogrammer_174_easy.py
@@ -0,0 +1,19 @@
+numrep=6
+iter=" "
+for x in range(numrep+1):  #loops over user input
+        for i in iter:
+               
+                if i == "0":
+                        iter = iter+"1"
+                
+                elif i == "1":
+                        iter = iter+"0"
+                
+                if iter == " ":
+                        iter ="0"
+                
+        print iter
+        
+
+
+
