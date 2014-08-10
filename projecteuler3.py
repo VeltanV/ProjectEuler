047cf4bf53ac04e67bb26c3c7feba64545fee9b8
bender662 <bender662@users.noreply.github.com>
2014-08-10 18:17:32 +0300

    problem3

--- projecteuler3.py
+++ projecteuler3.py
@@ -1,27 +1,42 @@
-fibo_list=[1,2]
-even_sumList=[]
-last_entry=0
-even_sum=0
-threshold=4000000
+from __future__ import division
+checkNum = 10
+primeList_num = []
+divList = []
+listLength=0
+
+def isPrime(number):
+    divList=[]
+    for x in range(1, number+1):
+
+        if number / x == 1 or number % x == 0 or number % x == number:
+
+            divList.append(x)
+
+           
+    
+            listLength=len(divList)
+
+    #print divList
+
+    if listLength<3:
+
+        return True
+
+    else:
+
+        return False
 
 
+for c in range(1,checkNum):
+        
+        if checkNum% c ==0:
+            if isPrime(c):
+                primeList_num.append(c)
 
-while last_entry<threshold:
+
+print primeList_num
 
         
-        if fibo_list[len(fibo_list)-1]%2==0:   #check if they have remainder
-
-            even_sumList.append(fibo_list[len(fibo_list)-1])
-        fibo_list.append(fibo_list[len(fibo_list)-1]+fibo_list[len(fibo_list)-2]) #add the sum of the previous 2
-        whole_sum=sum(fibo_list) 
-        even_sum=sum(even_sumList)
-        last_entry=fibo_list[len(fibo_list)-1]
-
-print even_sum
-print even_sumList
-print whole_sum
-
-
 
 
 
